4eb7dde8ee5159045d133da5bf71a0131b0f620b
toyama <shouya.toyama@gmail.com>
2023-02-03 15:49:10 +0000

    get memory map

---
+++ README.md
@@ -0,0 +1,4 @@
+# 「ゼロからのOS自作入門」作業リポジトリ
+
+## 参考
+[UEFI Specification](https://uefi.org/specs/UEFI/2.10/)
---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+workspace=/workspaces/mikanos_zig
+edk2=/home/vscode/edk2
+current=$(pwd)
+
+export CONF_PATH=${workspace}/edk2/Conf
+
+ln -sf ${workspace}/src/MikanLoaderPkg ${edk2}
+
+# configure edk2
+cd ${edk2}
+source edksetup.sh
+cd ${current}
+ln -sf ${edk2}/Build .
+
+# configure clang (kernel build)
+source /home/vscode/osbook/devenv/buildenv.sh
+
+unset -v workspace edk2 current
