ec331603e7e2bb8ed252e857432d432d76de2734
toyama <shouya.toyama@gmail.com>
2023-02-07 14:42:55 +0000

    implements PixelWriter

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## 参考
 [UEFI Specification](https://uefi.org/specs/UEFI/2.10/)
 [QEMU Monitor](https://en.wikibooks.org/wiki/QEMU/Monitor)
+[Easy Interfaces with Zig 0.10.0](https://zig.news/kristoff/easy-interfaces-with-zig-0100-2hc5)
--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -6,6 +6,53 @@ const PixelColor = struct {
     b: u8,
 };
 
+const PixelWriter = union(enum) {
+    rgb: RGBResv8BitPerColorPixelWriter,
+    bgr: BGRResv8BitPerColorPixelWriter,
+
+    pub fn pixelAt(config: *frame_buffer_config.FrameBufferConfig, x: usize, y: usize) [*]u8 {
+        const frame_buffer = @ptrCast([*]u8, config.frame_buffer);
+        return @ptrCast([*]u8, &frame_buffer[4 * (config.pixels_per_scan_line * y + x)]);
+    }
+    pub fn write(self: PixelWriter, x: usize, y: usize, c: *const PixelColor) void {
+        switch (self) {
+            inline else => |case| case.write(x, y, c),
+        }
+    }
+};
+
+const RGBResv8BitPerColorPixelWriter = struct {
+    config: *frame_buffer_config.FrameBufferConfig,
+
+    pub fn init(config: *frame_buffer_config.FrameBufferConfig) RGBResv8BitPerColorPixelWriter {
+        return RGBResv8BitPerColorPixelWriter{
+            .config = config,
+        };
+    }
+    pub fn write(self: RGBResv8BitPerColorPixelWriter, x: usize, y: usize, c: *const PixelColor) void {
+        var p = PixelWriter.pixelAt(self.config, x, y);
+        p[0] = c.r;
+        p[1] = c.g;
+        p[2] = c.b;
+    }
+};
+
+const BGRResv8BitPerColorPixelWriter = struct {
+    config: *frame_buffer_config.FrameBufferConfig,
+
+    pub fn init(config: *frame_buffer_config.FrameBufferConfig) BGRResv8BitPerColorPixelWriter {
+        return BGRResv8BitPerColorPixelWriter{
+            .config = config,
+        };
+    }
+    pub fn write(self: BGRResv8BitPerColorPixelWriter, x: usize, y: usize, c: *const PixelColor) void {
+        var p = PixelWriter.pixelAt(self.config, x, y);
+        p[0] = c.b;
+        p[1] = c.g;
+        p[2] = c.r;
+    }
+};
+
 fn WritePixel(config: *frame_buffer_config.FrameBufferConfig, x: usize, y: usize, c: *const PixelColor) i32 {
     const frame_buffer = @ptrCast([*]u8, config.frame_buffer);
     const pixel_position = config.pixels_per_scan_line * y + x;
@@ -25,11 +72,16 @@ fn WritePixel(config: *frame_buffer_config.FrameBufferConfig, x: usize, y: usize
 }
 
 export fn KernelMain(config: *frame_buffer_config.FrameBufferConfig) void {
+    var pixel_writer: *PixelWriter = switch (config.pixel_format) {
+        .kPixelRGBResv8BitPerColor => &PixelWriter{ .rgb = RGBResv8BitPerColorPixelWriter.init(config) },
+        .kPixelBGRResv8BitPerColor => &PixelWriter{ .bgr = BGRResv8BitPerColorPixelWriter.init(config) },
+    };
+
     var x: usize = 0;
     var y: usize = 0;
     while (x < config.horizontal_resolution) : (x += 1) {
         while (y < config.vertical_resolution) : (y += 1) {
-            _ = WritePixel(config, x, y, &PixelColor{ .r = 255, .g = 255, .b = 255 });
+            pixel_writer.write(x, y, &PixelColor{ .r = 255, .g = 255, .b = 255 });
         } else {
             y = 0;
         }
@@ -37,8 +89,8 @@ export fn KernelMain(config: *frame_buffer_config.FrameBufferConfig) void {
     x = 0;
     y = 0;
     while (x < 200) : (x += 1) {
-        while (y < 200) : (y += 1) {
-            _ = WritePixel(config, x, y, &PixelColor{ .r = 0, .g = 255, .b = 0 });
+        while (y < 300) : (y += 1) {
+            pixel_writer.write(x, y, &PixelColor{ .r = 0, .g = 255, .b = 0 });
         } else {
             y = 0;
         }
